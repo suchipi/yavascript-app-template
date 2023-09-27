71bfe57c0a2c7316f0012a59dca1c16610aa267c
Lily Skye <me@suchipi.com>
2023-09-26 22:52:42 -0600

    bump kame (which removes need for config)

--- meta/kame.config.ts
+++
@@ -1,9 +0,0 @@
-import { defaultResolver } from "kame";
-
-export const resolve = (id: string, fromFilePath: string) => {
-  if (id.startsWith("quickjs:")) {
-    return "external:" + id;
-  }
-
-  return defaultResolver.resolve(id, fromFilePath);
-};
